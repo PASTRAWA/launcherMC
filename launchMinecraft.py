6784b8e4dab4139ddee77ce08f4e45de1c64a7e5
PASTRAWA <126355129+PASTRAWA@users.noreply.github.com>
2026-02-11 23:29:14 +0300

    xx

--- launchMinecraft.py
+++
@@ -1,5 +0,0 @@
-import minecraft_launcher_lib 
-import subprocess
-def launchMc(version, directory, options):
-    startMC = minecraft_launcher_lib.command.get_minecraft_command("1.8.9", directory, options)
-    subprocess.call(startMC)
